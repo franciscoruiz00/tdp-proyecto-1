b4d7e10cc267f232bff33f1fac2f53d8b63c81c0
franciscoruiz00 <franciscoruiz@hotmail.com>
2021-08-31 11:00:16 -0300

    Inicio repositorio con codigo fuente base y gitignore

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -1,21 +1,24 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import entities.Student;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Image;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.awt.Toolkit;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -24,8 +27,15 @@ public class SimplePresentationScreen extends JFrame {
 	private JPanel tabInformation;
 	private JTabbedPane tabbedPane;
 	private Student studentData;
+	private JTextField textFieldLU;
+	private JTextField textFieldApellido;
+	private JTextField textFieldName;
+	private JTextField textFieldMail;
+	private JTextField textFieldGit;
+	
 
 	public SimplePresentationScreen(Student studentData) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SimplePresentationScreen.class.getResource("/images/tdp.png")));
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
@@ -34,9 +44,11 @@ public class SimplePresentationScreen extends JFrame {
 		
 		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentacion");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(new Dimension(650, 341));
+		setSize(new Dimension(580, 289));
 		setResizable(false);
 		setContentPane(contentPane);
+		
+	
 		
 		init();
 	}
@@ -45,8 +57,92 @@ public class SimplePresentationScreen extends JFrame {
 		// Tabbed Pane to student personal data
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabInformation = new JPanel();
-		tabInformation.setPreferredSize(new Dimension(425, 275));
+		tabInformation.setPreferredSize(new Dimension(425, 20));
 		tabbedPane.addTab("Informacion del alumno", null, tabInformation, "Muestra la informacion declarada por el alumno");
+		tabInformation.setLayout(null);
+		
+		
+		JLabel lblLu = new JLabel("LU");
+		lblLu.setFont(new Font("SansSerif", Font.BOLD, 15));
+		tabInformation.add(lblLu);
 		contentPane.add(tabbedPane, BorderLayout.WEST);
+		lblLu.setBounds(1, 10, 62, 38);
+		
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setFont(new Font("SansSerif", Font.BOLD, 15));
+		lblApellido.setBounds(1, 42, 62, 38);
+		tabInformation.add(lblApellido);
+		
+		JLabel lblName = new JLabel("Nombre");
+		lblName.setFont(new Font("SansSerif", Font.BOLD, 15));
+		lblName.setBounds(1, 77, 62, 38);
+		tabInformation.add(lblName);
+		
+		JLabel lblMail = new JLabel("E-mail");
+		lblMail.setFont(new Font("SansSerif", Font.BOLD, 15));
+		lblMail.setBounds(1, 112, 62, 38);
+		tabInformation.add(lblMail);
+		
+		JLabel lblGit = new JLabel("GitHub URL");
+		lblGit.setFont(new Font("SansSerif", Font.BOLD, 15));
+		lblGit.setBounds(1, 145, 91, 40);
+		tabInformation.add(lblGit);
+		
+		JLabel lblIma = new JLabel();
+		lblIma.setBounds(445, 30, 120, 159);
+		ImageIcon ima = new ImageIcon(SimplePresentationScreen.class.getResource(studentData.getPathPhoto()));
+		Icon ima2 = new ImageIcon(ima.getImage().getScaledInstance(lblIma.getWidth(), lblIma.getHeight(), Image.SCALE_DEFAULT));
+		contentPane.add(lblIma);
+		lblIma.setIcon(ima2);
+
+		DateTimeFormatter time1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter time2 = DateTimeFormatter.ofPattern("HH:mm:ss");
+		JLabel lblTime = new JLabel("Esta ventana fue generada el " + time1.format(LocalDateTime.now()) + " a las: "+time2.format(LocalDateTime.now()));
+		lblTime.setFont(new Font("SansSerif", Font.BOLD, 14));
+		contentPane.add(lblTime, BorderLayout.SOUTH);
+		
+		
+		textFieldLU = new JTextField();
+		textFieldLU.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		textFieldLU.setBounds(93, 21, 322, 20);
+		textFieldLU.setText(String.valueOf(studentData.getId()));
+		tabInformation.add(textFieldLU);
+		textFieldLU.setColumns(10);
+		
+		textFieldApellido = new JTextField();
+		textFieldApellido.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		textFieldApellido.setBounds(93, 53, 322, 20);
+		textFieldApellido.setText(studentData.getLastName());
+		tabInformation.add(textFieldApellido);
+		textFieldApellido.setColumns(10);
+		
+		textFieldName = new JTextField();
+		textFieldName.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		textFieldName.setBounds(93, 88, 322, 20);
+		textFieldName.setText(studentData.getFirstName());
+		tabInformation.add(textFieldName);
+		textFieldName.setColumns(10);
+		
+		textFieldMail = new JTextField();
+		textFieldMail.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		textFieldMail.setBounds(93, 123, 322, 20);
+		textFieldMail.setText(studentData.getMail());
+		tabInformation.add(textFieldMail);
+		textFieldMail.setColumns(10);
+		
+		
+		textFieldGit = new JTextField();
+		textFieldGit.setFont(new Font("SansSerif", Font.PLAIN, 14));
+		textFieldGit.setBounds(93, 156, 322, 20);
+		textFieldGit.setText(studentData.getGithubURL());
+		tabInformation.add(textFieldGit);
+		textFieldGit.setColumns(10);
+
+	
+	
+		
+		
+	
+	
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,21 @@
 package launcher;
 
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	try {
+            		 Student estud = new Student(121666,"Ruiz Gomez","Francisco","franciscoruizg00@hotmail.com","https://github.com/franciscoruiz00","/images/imagen.jpg");
+            				SimplePresentationScreen window = new SimplePresentationScreen(estud);
+            		 window.setVisible(true);
+            	}catch (Exception e) {
+            	e.printStackTrace();
+            	}
             }
         });
     }
